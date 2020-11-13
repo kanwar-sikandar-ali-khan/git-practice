f855a079775cf0130d163ca53b0b95d9769b713e
kanwar-sikandar-ali-khan <kanwarsikandarali>
2020-11-13 06:18:32 -0800

    new commit

---
+++ src/components/name.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class Name extends Component {
+    render() {
+        return (
+            <div>
+
+                <h1 style={{color:'black'}}>created by: KANWAR SIKANDAR  ALI KHAN</h1>
+                
+            </div>
+        )
+    }
+}
+
+export default Name;
